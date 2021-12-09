4e9efa2d8bcd081537d46b6c3173edf9846079e0
abba123 <a0981861951@gmail.com>
2021-12-09 16:41:41 +0800

    add analysis

---
+++ vue/web/src/components/analysis.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+      
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'analysis',
+  data () {
+    return {
+      msg: 'Welcome to PokerManager',
+    }
+  },
+  methods:{
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
